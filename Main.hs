48c112cb02d70ddc9e8f37782d975c96b2f7e74f
Matthías Páll Gissurarson <mpg@mpg.is>
2017-10-09 00:13:18 +0200

    Prettify a bit

--- Main.hs
+++ Main.hs
@@ -11,8 +11,6 @@
 module Main where
 
 import Sized
-
-import GHC.TypeLits
 import Data.Proxy
 
 
@@ -28,11 +26,11 @@ three = wrapWithSize $ putStrLn "three"
 -- When doing one operation after another,
 -- We add the cost of the operations together
 six :: SizedIO 6 ()
-six = wrapWithExplicitSize (Proxy :: Proxy 0) (putStrLn "sum of: ")
-  Sized.>>= \_ -> one
-  Sized.>>= \_ -> two
-  Sized.>>= \_ -> three
-  Sized.>>= \_ -> wrapWithExplicitSize (Proxy :: Proxy 0) $ putStrLn "is six"
+six = (wrapWithExplicitSize (Proxy :: Proxy 0) (putStrLn "sum of: "))
+  |>> one
+  |>> two
+  |>> three
+  |>> (wrapWithExplicitSize (Proxy :: Proxy 0) $ putStrLn "is six")
 
 t1 = six
 
@@ -40,14 +38,14 @@ t1 = six
 -- maximum of each side.
 t2 :: SizedIO 6 ()
 t2 = wrapWithExplicitSize (Proxy :: Proxy 0) (putStrLn "the max of:")
-  Sized.>>= \_ -> (\_ _ -> ()) Sized.<$>  three Sized.<*> six
-  Sized.>>= \_ -> wrapWithExplicitSize (Proxy :: Proxy 0) (putStrLn "is six!")
+  |>> (\_ _ -> ()) |<$> three |<*> six
+  |>> (wrapWithExplicitSize (Proxy :: Proxy 0) (putStrLn "is six!"))
 
 -- We can also put an upper bound on the size,
 -- which is useful for e.g. development.
 t3 :: SizedIO 8 ()
 t3 = isAtMost (Proxy :: Proxy 8) t2
-   Sized.>>= \_ -> wrapWithSize $ putStrLn "which is less than eight!"
+   |>> (wrapWithSize $ putStrLn "which is less than eight!")
 
 
 -- Main.hs:56:14: error:
--- Sized.hs
+++ Sized.hs
@@ -21,20 +21,27 @@ import Data.Type.Equality
 type family Max (a :: Nat) (b :: Nat) where
   Max a b = If (CmpNat a b == 'LT) b a
 
+infixl 1  |>>, |>>=
+infixl 4 |<*>, |<$>
+
 class KnownNat s => SizedFunctor f s where
   fmap :: (a -> b) -> f s a -> f s b
 
 -- | A synonym for fmap
-(<$>) :: SizedFunctor f s =>  (a -> b) -> f s a -> f s b
-(<$>) = Sized.fmap
+(|<$>) :: SizedFunctor f s =>  (a -> b) -> f s a -> f s b
+(|<$>) = Sized.fmap
 
 class (SizedFunctor f s) => SizedApplicative f s  where
   pure :: a -> f s a
-  (<*>) :: (SizedApplicative f t, SizedApplicative f v, s ~ Max t v) => f t (a -> b) -> f v a -> f s b
+  (|<*>) :: (SizedApplicative f t, SizedApplicative f v, s ~ Max t v) => f t (a -> b) -> f v a -> f s b
+
 
 class (KnownNat s, SizedApplicative m s) => SizedMonad m s where
   return :: SizedMonad m s => a -> m s a
-  (>>=) :: SizedMonad m t => m s a -> (a -> m t b) -> m (s + t) b
+  (|>>=) :: SizedMonad m t => m s a -> (a -> m t b) -> m (s + t) b
+
+(|>>) :: (SizedMonad m s, SizedMonad m t) => m s a -> m t b -> m (s + t) b
+a |>> b = a |>>= (\_ -> b)
 
 data Vec n a where
   Nil :: Vec 0 a
@@ -67,14 +74,13 @@ instance (KnownNat n, Functor f) => SizedFunctor (SizedT f) n where
 
 instance (KnownNat n, Applicative f) => SizedApplicative (SizedT f) n where
   pure = wrapWithSize . Prelude.pure
-  a <*> b = wrapWithSize $ (runSizedT a) Prelude.<*> (runSizedT b)
+  a |<*> b = wrapWithSize $ (runSizedT a) <*> (runSizedT b)
 
 instance (KnownNat n, Monad m) => SizedMonad (SizedT m) n where
   return = wrapWithSize . Prelude.return
-  a >>= b = wrapWithSize $ (runSizedT a) Prelude.>>= (\k -> runSizedT $ b k)
+  a |>>= b = wrapWithSize $ (runSizedT a) >>= (\k -> runSizedT $ b k)
 
 
 type SizedIO = SizedT IO
-
 
 
